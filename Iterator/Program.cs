f91fb38cfa88d1f3ebd9123db2a8b3526ff310e5
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2022-11-27 21:55:27 +0400

    Add Iterator

--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -1,2 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+
+namespace Iterator;
+
+struct Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+}
+
+interface IAggregate
+{
+    IIterator CreateIterator();
+}
+
+interface IIterator
+{
+    bool HasItem();
+    Employee NextItem();
+    Employee CurrentItem();
+    void Reset();
+}
+
+class EmployeeAggregate : IAggregate
+{
+    List<Employee> Employees = new List<Employee>();
+    public void Add(Employee Model) => Employees.Add(Model);
+    public Employee GetItem(int index) => Employees[index];
+    public int Count { get => Employees.Count; }
+    public IIterator CreateIterator() => new EmployeeIterator(this);
+}
+
+
+class EmployeeIterator : IIterator
+{
+    EmployeeAggregate aggregate;
+    int currentindex;
+    public EmployeeIterator(EmployeeAggregate aggregate) => this.aggregate = aggregate;
+    public Employee CurrentItem() => aggregate.GetItem(currentindex);
+    public bool HasItem()
+    {
+        if (currentindex < aggregate.Count)
+            return true;
+        return false;
+    }
+    public Employee NextItem()
+    {
+        if (HasItem())
+            return aggregate.GetItem(currentindex++);
+        return new Employee();
+    }
+
+    public void Reset()
+    {
+        currentindex = 0;
+    }
+}
+
+class Program
+{
+
+    static void Main()
+    {
+        EmployeeAggregate aggregate = new EmployeeAggregate();
+        aggregate.Add(new Employee { Id = 1, Name = "Gençay", Surname = "Yıldız" });
+        aggregate.Add(new Employee { Id = 2, Name = "Ahmet", Surname = "Çakmak" });
+        aggregate.Add(new Employee { Id = 3, Name = "Huseyn", Surname = "Hemidov" });
+        aggregate.Add(new Employee { Id = 4, Name = "Emin", Surname = "Novruz" });
+
+        IIterator iterator = aggregate.CreateIterator();
+        while (iterator.HasItem())
+        {
+            Console.WriteLine($"ID : {iterator.CurrentItem().Id}\nName : {iterator.CurrentItem().Name}\nSurname : {iterator.CurrentItem().Surname}\n*****");
+            iterator.NextItem();
+        }
+        iterator.Reset();
+        while (iterator.HasItem())
+        {
+            Console.WriteLine($"ID : {iterator.CurrentItem().Id}\nName : {iterator.CurrentItem().Name}\nSurname : {iterator.CurrentItem().Surname}\n*****");
+            iterator.NextItem();
+        }
+        //Console.WriteLine(iterator.HasItem());
+
+    }
+}
