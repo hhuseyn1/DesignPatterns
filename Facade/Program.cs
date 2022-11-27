0410d5109588171fae3f6e084bc5f9ad16764a96
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2022-11-27 21:56:03 +0400

    Add Facade

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Facade;
+
+public class Product
+{
+    public void GetProductDetails()
+    {
+        Console.WriteLine("Product details");
+    }
+}
+
+public class Payment
+{
+    public void MakePayment()
+    {
+        Console.WriteLine("Payment successfully");
+    }
+}
+
+public class Invoice
+{
+    public void Sendinvoice()
+    {
+        Console.WriteLine("Invoice successfully");
+    }
+}
+public class OrderFacade
+{
+    private Product _product = new();
+    private Payment _payment = new();
+    private Invoice _invoice = new();
+    public void PlaceOrder()
+    {
+        Console.WriteLine("Place order started");
+        _product.GetProductDetails();
+        _payment.MakePayment();
+        _invoice.Sendinvoice();
+        Console.WriteLine("Order placed successfully");
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        OrderFacade order = new OrderFacade();
+        order.PlaceOrder();
+    }
+}
